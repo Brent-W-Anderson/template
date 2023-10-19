82d9043bf7327c8d1c56a5ab271c572741e87c52
J-A-R-V-I-S\gobre <gobrent21@gmail.com>
2023-10-18 21:58:48 -0500

    add source maps for dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' )
 module.exports = {
     mode: process.env.WEBPACK_MODE || 'development',
     entry: './src/index',
+    devtool: 'source-map',
     output: {
         path: path.resolve( __dirname, 'dist' ),
         filename: '[name].[contenthash].js'
