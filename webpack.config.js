d36c4e488028782622fc03a140a8ace87222c9e0
J-A-R-V-I-S\gobre <gobrent21@gmail.com>
2023-10-22 15:53:23 -0500

    remove file-loader because it interferes with SVGR

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
             },
             {
                 test: /\.svg$/,
-                use: ['@svgr/webpack', 'file-loader'],
+                use: ['@svgr/webpack'],
             },
         ],
     },
