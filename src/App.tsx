5577e4831234325d43737bbb16484b914122f7e5
J-A-R-V-I-S\gobre <gobrent21@gmail.com>
2023-10-22 15:57:38 -0500

    remove TODO

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import style from './assets/scss/globals/globals.module.scss'
 import HomePage from './pages/HomePage/HomePage'
 
 // TODO: REACT-ROUTER/ LAZY LOADING COMPONENTS/ README
-// TODO: FORMAT TO THE NEXT LINE AUTOMATICALLY ON SAVE (PRETTIER?)
 const App = (): React.JSX.Element => {
     return (
         <div className={style.app}>
